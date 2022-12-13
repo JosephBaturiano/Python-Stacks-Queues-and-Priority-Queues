d011b8f791f39a521e0401c33b9c492a6f39cde8
JosephBaturiano <116430462+JosephBaturiano@users.noreply.github.com>
2022-12-13 15:24:21 +0630

    printing the three elements and will dequeued 1by1

--- lifostack1.py
+++ lifostack1.py
@@ -4,3 +4,7 @@ lifo.append("2nd")
 lifo.append("3rd")
 
 print(lifo)
+
+print("The ",lifo.pop()," element has been Dequeued",lifo )
+print("The ",lifo.pop()," element has been Dequeued",lifo )
+print("The ",lifo.pop()," element has been Dequeued",lifo )
