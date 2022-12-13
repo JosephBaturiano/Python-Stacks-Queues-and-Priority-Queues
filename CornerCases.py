80cef8276d908144b11c99a32e4cc1834ed66bf9
Joseph Z. Baturiano <josephbaturiano3@gmail.com>
2022-12-13 23:38:02 +0630

    Use the dataclass module and declare a two variable with a message syntax on it

--- CornerCases.py
+++ CornerCases.py
@@ -4,3 +4,12 @@ from dataclasses import dataclass
 CRITICAL = 3
 IMPORTANT = 2
 NEUTRAL = 1
+
+
+@dataclass
+class Message:
+    event: str
+
+
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
