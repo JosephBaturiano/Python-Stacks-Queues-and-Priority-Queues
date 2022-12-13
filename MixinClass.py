d7defc0a604b01bac085be811ec195debfaeb114
Joseph Z. Baturiano <josephbaturiano3@gmail.com>
2022-12-14 01:18:16 +0630

    Inserted the IterableMixin class

--- MixinClass.py
+++ MixinClass.py
@@ -3,16 +3,18 @@ from heapq import heappop, heappush
 from itertools import count
 
 
-class Queue:
-    def __init__(self, *elements):
-        self._elements = deque(elements)
-
+class IterableMixin:
     def __len__(self):
         return len(self._elements)
 
     def __iter__(self):
         while len(self) > 0:
             yield self.dequeue()
+
+
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
 
     def enqueue(self, element):
         self._elements.append(element)
@@ -26,7 +28,7 @@ class Stack(Queue):
         return self._elements.pop()
 
 
-class PriorityQueue:
+class PriorityQueue(IterableMixin):
     def __init__(self):
         self._elements = []
         self._counter = count()
