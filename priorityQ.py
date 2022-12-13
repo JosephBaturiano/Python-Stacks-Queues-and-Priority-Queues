37972753657020c0161d4d09b15e34b80d5eae1c
Joseph Z. Baturiano <josephbaturiano3@gmail.com>
2022-12-13 19:55:09 +0630

    dequeueing the highest priority but ended getting the lowest prio

--- priorityQ.py
+++ priorityQ.py
@@ -9,3 +9,5 @@ messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
 messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
 messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
 messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue())
