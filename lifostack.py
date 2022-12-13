84d107198f54612b1fea0be113b7b309e78c741e
Joseph Z. Baturiano <josephbaturiano3@gmail.com>
2022-12-13 18:36:11 +0630

    correcting the code

--- lifostack.py
+++ lifostack.py
@@ -5,9 +5,5 @@ print("Nos of stack:", len(lifo))
 
 for element in lifo:
     print(element)
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> d011b8f791f39a521e0401c33b9c492a6f39cde8
 print("Current number of stack:", len(lifo))
